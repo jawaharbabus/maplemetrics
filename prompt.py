6a1c5daf1ad3733edcec38ea99f99721048e74ea
Jawahar Babu Selvaraj <jawaharbabu199914@gmail.com>
2025-10-04 22:25:35 -0400

    Initial commit: Add Python app with gitignore

---
+++ prompt.py
@@ -0,0 +1,6 @@
+prompt = '''
+
+
+Can you just check if the stat can has dataset for Ontario agriculture. just check, dont retrieve data or analyse or plot.
+Try not to call 'search_cubes_by_title'  
+'''
---
+++ test_client.py
@@ -0,0 +1,54 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from langchain.agents import AgentExecutor
+from langchain_openai import ChatOpenAI
+import os
+import asyncio
+from prompt import prompt
+
+async def main():
+    client = MultiServerMCPClient(
+        {
+            "chart": {
+            "url": "http://localhost:1122/mcp",   # check port after running server
+            "transport": "streamable_http"
+        },
+        "statcan": {
+            "command": "uv",
+            "args": [
+            "run",
+            "--with", "fastmcp",
+            "--with", "httpx", 
+            "sh",
+            "-c",
+            "cd ~/Documents/project/opensrc_proj/mcp-statcan && python -m src.server"
+            ],
+        "transport": "stdio"
+        }
+        
+})
+    tools = await client.get_tools()
+    model = ChatOpenAI(
+        model="gpt-5",
+        api_key=os.getenv("OPENAI_KEY")
+    )
+    agent = create_react_agent(
+        model,
+        tools
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    # Example query: ask agent to make a chart
+    resp = await agent_executor.ainvoke({
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+                }]
+    })
+    print(resp)
+    
+if __name__ == "__main__":
+    
+    asyncio.run(main())
